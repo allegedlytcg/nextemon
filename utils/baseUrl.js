9407dc568c06a85a4ba708d00aaa1db9afaf3782
unknown <jt.mccutcheon@live.com>
2021-05-16 18:56:48 -0400

    buster

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,4 +1,4 @@
 export const baseUrl =
 	process.env.NODE_ENV !== 'production'
-		? 'http://localhost:6969/api/v1'
+		? 'http://localhost:8080/api/v1'
 		: 'https://allegedlytcg.herokuapp.com/api/v1';
