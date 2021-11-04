8f7e67cf1789b5625de8c4c8e567f317119ed88e
unknown <jt.mccutcheon@live.com>
2021-11-03 21:49:18 -0400

    create and fetch game

--- gamelogic/__tests__/getPrizeCards.test.js
+++ gamelogic/__tests__/getPrizeCards.test.js
@@ -15,19 +15,15 @@ test('should get top 6 cards of deck', () => {
 		],
 	};
 
-	expect(getPrizeCards(deck.cards)).toStrictEqual({
-		prizeCards: [
-			{ name: 'charmeleon' },
-			{ name: 'wartortle' },
-			{ name: 'mewtwo' },
-			{ name: 'tentacruel' },
-			{ name: 'aerodactyl' },
-			{ name: 'omanyte' },
-		],
-		updatedDeck: [
-			{ name: 'slowpoke' },
-			{ name: 'pigeot' },
-			{ name: 'arbok' },
-		],
-	});
+	expect(getPrizeCards(deck.cards)).toStrictEqual([
+		{ name: 'slowpoke' },
+		{ name: 'pigeot' },
+		{ name: 'arbok' },
+		{ isPrizeCard: true, name: 'charmeleon' },
+		{ isPrizeCard: true, name: 'wartortle' },
+		{ isPrizeCard: true, name: 'mewtwo' },
+		{ isPrizeCard: true, name: 'tentacruel' },
+		{ isPrizeCard: true, name: 'aerodactyl' },
+		{ isPrizeCard: true, name: 'omanyte' },
+	]);
 });
