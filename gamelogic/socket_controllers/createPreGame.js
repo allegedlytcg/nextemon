543a8c7192de64cffedc5324cf0747552c6196b3
Ashley Owens <ash4741@gmail.com>
2022-01-11 23:40:59 -0500

    Update getDeckById Function to Returns Deck

--- gamelogic/socket_controllers/createPreGame.js
+++ gamelogic/socket_controllers/createPreGame.js
@@ -1,17 +1,13 @@
 require('dotenv').config({ path: require('find-config')('.env') });
 const { MongoClient } = require('mongodb');
-const pregameDoc = require('../sample_docs/pregame.json');
 
 //TODO remove creating preGame obj hopefully with sample pregame.json and use args passed instead from server on 2nd socket join of room
-
 
 //auto deletes a previous pregame with the same room name, assumes room control logic is in place to handle pregame obj mishaps
 //room id=string players=[]
 const createPreGame = ( roomId,  players ) => {
-	console.log("alright createPregame started...")
 	try {
 		// let roomId = "hibidee-dibidee";
-		console.debug("pregame doc is " + JSON.stringify(pregameDoc) + "and is of type: " + typeof(pregameDoc));
 		let players1 = 	{"players": [
 			{
 				"socketId": players[0],
@@ -31,42 +27,28 @@ const createPreGame = ( roomId,  players ) => {
 				const pregameDb = db.db('test');
 
 				const collection = pregameDb.collection('pregames');
-				let collectionCount = collection.find();
-				let countcount = await collectionCount.count();
-				console.log("ALL PREGAMES IN COLLECTION COUNT IS: " + JSON.stringify(countcount));
 				const existing = await collection.findOne({ roomId });
-				console.debug("if not null exists + " +  JSON.stringify(existing));
 
 				if (existing !== null) {
-					//TODO DELETE IT and recreate instead eventually
-					console.debug("DELETING PREVIOUS ROOM NAME WAS USED + " +  existing);
-
 					const delres = collection.deleteOne(existing);
 					//if above doesn't work delete using roomId?
 					
-					console.log("DELETING PREGAME with roomId: "+ roomId +" aleady exists, needs deletion/forced update RESULTED is" + JSON.stringify(delres));
+					// console.log("DELETING PREGAME with roomId:", roomId, "aleady exists, needs deletion/forced update RESULTED is", JSON.stringify(delres));
 					//now existing should be null
 					const newexisting = await collection.findOne({roomId});
-					console.debug("Replaced deleted with new pregame config using same name! + " +  newexisting);
+					// console.debug("Replaced deleted with new pregame config using same name!", newexisting);
 				}
-				else{
-					console.log('ROOM NOT CREATED YET')
-				}
-				//else(implied from return statement above)
 
 				const newPreGame = {
 					roomId,
 					players: players1,
 				};
-				console.log('log after NEWPREGAME with value of: '+ JSON.stringify(newPreGame));
-				await collection.insertOne(newPreGame);
 
+				await collection.insertOne(newPreGame);
 				db.close();
 			},
 		);
-		return {
-			gameStatus: 'PREGAME_CREATE_SUCCESS',
-		};
+		return {gameStatus: 'PREGAME_CREATE_SUCCESS'};
 	} catch (error) {
 		return error;
 	}
--- gamelogic/socket_controllers/updatePreGame.js
+++ gamelogic/socket_controllers/updatePreGame.js
@@ -1,45 +1,18 @@
 require('dotenv').config({ path: require('find-config')('.env') });
-
-const { MongoClient, ObjectID } = require('mongodb');
-// const Deck = require('../../models/Deck');
-
-
- const updatePreGame =  (roomId, player, deckToFind) => {
-	console.log("deckTofind '_id' for 'decks' is: " + JSON.stringify(deckToFind));
-	return getDeckById(deckToFind)
-}
+const Deck = require('../../models/Deck')
 
 //local get deck by id request
 async function getDeckById(deckIdPassed){
 	try {
-		
-		return MongoClient.connect(
-			process.env.MONGO_DB,
-			{ useUnifiedTopology: true },
-			async (err, db) => {
-				if (err) throw err;
-				const mainDb = db.db('test');
-
-				const collection = mainDb.collection('decks');
-				let collectionCount = collection.find();
-				let countcount = await collectionCount.count();
-				console.log("deck ID just befoRE QUERY" + JSON.stringify(deckIdPassed));
-				console.log("ALL decks IN COLLECTION COUNT IS: " + (countcount));
-				let result = await collection.findOne({ _id: ObjectID(`${deckIdPassed}`) });
-		        console.log("result of local FINDDECK  is" + JSON.stringify(result));
-				db.close()
-				return result ;
-
-			}
-			
-			);
-
-
+		const deck = await Deck.findById(deckIdPassed)
+		return deck
 	} catch (error) {
 		return error;
 	}
- 
+}
 
+const updatePreGame =  (roomId, player, deckToFind) => {
+	return getDeckById(deckToFind)
 }
 
 module.exports = { updatePreGame };
