e71d11d469ade3c4c547a89135dce56e5325cb74
Char Erd <charley_43613@hotmail.com>
2022-01-23 22:55:03 -0500

    update working correctly, both decks updated, ready for perspective/coin toss decision'

--- gamelogic/socket_controllers/updatePreGame.js
+++ gamelogic/socket_controllers/updatePreGame.js
@@ -5,43 +5,60 @@ const Pregame = require('../../models/Pregame')
 //local get deck by id request
 async function updateGameConfig(roomId, player, deckIdPassed){
 	try {
-		console.log("ROOMID sent with updategameconfig is " +roomId);
+		console.log("ROOMID sent with updategameconfig is " +roomId + " while player si " + player);
 		const deck = await Deck.findById(deckIdPassed)
 		// console.log("deck found IN UPDATE GAMECONFIG is..." + JSON.stringify(deck.name));
 		//TODO replace with findOneandUpdate
 		// console.log("roomId PASSED ot find the fucking game config is " + JSON.stringify(roomId));
-		const tempPregame = await Pregame.findOne({roomId});
-		console.log("temppregame config is " + JSON.stringify(tempPregame.roomId) + "while player socket is " + JSON.stringify(player));;
+		const tempPregame = await Pregame.findOne({roomId, "players.socketId": player });
+		console.log("temppregame config is " + JSON.stringify(tempPregame) + "while player socket is " + JSON.stringify(player));;
+		let isempty = false;
+		if(tempPregame.players[0].cards.length >0){
+			console.log("splice of player 0 cards is " + JSON.stringify(tempPregame.players[0].cards.slice(-2)))
+		}
+		else{
+			console.log("instead of cards temppregameat player 0 is " + tempPregame.players[0]);
+		}
+		if(tempPregame.players[1].cards.length >0){
+			console.log("splice of player 0 cards is " + JSON.stringify(tempPregame.players[1].cards.slice(-2)))
+		}
+		else{
+			console.log("instead of cards temppregameat player 1 is " + tempPregame.players[1]);
+		}
 		let indexOfPlayer = 0;
 		//get position of the index
-		for (let i = 0; i < tempPregame.players.length; i++) {
-			if (player === tempPregame.players[i].socketId){
-				//found matching socket	
-				indexOfPlayer = i;
-				break;
-			}
-			else if(i+1 === tempPregame.players.length){
-				console.error("did not find appropriate socket ID for pregame configuration, there is an issue on server side");
-			}
-		  }
-		let body = JSON.parse(JSON.stringify(tempPregame));
+		// for (let i = 0; i < tempPregame.players.length; i++) {
+		// 	if (player === tempPregame.players[i].socketId){
+		// 		//found matching socket	
+		// 		indexOfPlayer = i;
+		// 		break;
+		// 	}
+		// 	else if(i+1 === tempPregame.players.length){
+		// 		console.error("did not find appropriate socket ID for pregame configuration, there is an issue on server side");
+		// 	}
+		//   }
+		//   console.log("index of player found is" + indexOfPlayer);
+		// let body = JSON.parse(JSON.stringify(tempPregame));
 		// console.log("deck to stringify maybe is " + JSON.stringify(deck));
-		body.players[indexOfPlayer].cards = JSON.parse(JSON.stringify(deck.cards));
-		delete body._id;
-		let sliceOfcards = body.players[indexOfPlayer].cards.slice(-2);
+		let cardsToUpdate = JSON.parse(JSON.stringify(deck.cards));
+	
+		let sliceOfcards = cardsToUpdate.slice(-2);
+		let indexOfOther = indexOfPlayer===0?1:0;
+		// let sliceOfcardsOtherPlayer = body.players[indexOfOther].cards.length >0? body.players[indexOfOther].cards.slice(-2):" No cards found!";
 
 		console.log("body is now hopefully sent with update here as..." + JSON.stringify(sliceOfcards) );
-		
+		// console.log("Other player cards also in the body sent for update?" + JSON.stringify(sliceOfcardsOtherPlayer))
 
 		const pregameUpdated = await Pregame.findOneAndUpdate(
-			{ roomId },
-			body,
-			{ new: true },
+			{ roomId, "players.socketId": player },
+			{ $set: { "players.$.cards": cardsToUpdate}}
+			
 		);
+		
 		console.log("pregame updated and found is..." + JSON.stringify(pregameUpdated.roomId));
 		console.log("setting player config and finding out if not empty on pregame record after update");
 		// let returnString = "Issue occured during update pregame config";
-
+			console.log("pregameupdated result is " + pregameUpdated);
 		return pregameUpdated
 		
 	} catch (error) {
